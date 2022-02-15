0abf97e5d3e848e535a35e0ca6dcb229ba7d74ce
roehrt <roehrt@users.noreply.github.com>
2022-02-15 20:31:15 +0100

    Move to typescript

---
+++ index.ts
@@ -0,0 +1,252 @@
+/**
+ * This small package contains the implementation
+ * of a trick which allows to save money when
+ * booking a trip on {@link https://bahn.de|bahn.de}.
+ *
+ * @projectname baahn
+ * @version 1.0.6
+ * @copyright 2022
+ *
+ * Finds cheaper journeys.
+ * @module baahn
+ */
+
+import hafasClient, {Journey, Journeys, JourneysOptions, Leg} from 'hafas-client';
+// @ts-ignore
+import dbProfile from 'hafas-client/p/db';
+// @ts-ignore
+import { data as loyaltyCards } from 'hafas-client/p/db/loyalty-cards';
+
+import adjacencyList from './static/adjacencyList.json';
+
+const { journeys } = hafasClient(dbProfile, 'baahn');
+
+/**
+ * A journey with an extra attribute `trick`
+ * which stores how the price saving was achieved
+ */
+interface BaahnJourney extends Journey {
+  trick?: {
+    prepend: Leg[],
+    append: Leg[],
+    oldPrice: number,
+  }
+}
+
+/**
+ * A string containing the eva code of a station.
+ */
+type BaahnStation = string;
+
+/**
+ * An identifier representing a specific journey based on it's stops (and the time of the stops).
+ */
+type BaahnJourneyHash = string;
+
+/**
+ * A lookup table for journeys.
+ */
+type BaahnJourneyMap = { [key: string]: BaahnJourney };
+
+/**
+ * An adjacency list representing the German long-distance
+ * train network.
+ */
+type StationGraph = { [key: string]: BaahnStation[] };
+
+
+/**
+ * Creates identifiable string from leg of a journey.
+ *
+ * @param {Leg} leg - leg of journey
+ * @returns {BaahnJourneyHash} hash
+ */
+function hashLeg(leg: Leg): BaahnJourneyHash {
+  return `${leg.origin?.id}@${leg.plannedDeparture ?? leg.departure}>`
+        + `${leg.destination?.id}@${leg.plannedArrival ?? leg.arrival}`;
+}
+
+/**
+ * Creates a hash from the legs of a journey.
+ *
+ * @param {Leg[]} legs - legs of journey
+ * @returns {BaahnJourneyHash} hash
+ */
+function hashLegs(legs: readonly Leg[]): BaahnJourneyHash {
+  return legs.map(hashLeg).join(':');
+}
+
+/**
+ * Returns adjacent stations in the German long-distance network.
+ *
+ * @param {BaahnStation} station - eva code of a station
+ * @returns {BaahnStation[]} adjacent stations
+ */
+function adjacentStations(station: BaahnStation): BaahnStation[] {
+  return (adjacencyList as StationGraph)[station] || [];
+}
+
+/**
+ * Updates the journey map if journey contains a cheaper price.
+ *
+ * @param {BaahnJourneyMap} journeyMap
+ * @param {BaahnJourney} journey
+ * @param {BaahnStation} from
+ * @param {BaahnStation} to
+ */
+function update(journeyMap: BaahnJourneyMap, journey: BaahnJourney, from: BaahnStation, to: BaahnStation) {
+  if (journey.price === null) return;
+
+  const { legs } = journey;
+
+  // Remove the extension from the journey
+  const prepend = [];
+  while (legs.length && legs[0].origin?.id !== from) {
+    // @ts-ignore
+    prepend.push(legs.shift());
+  }
+
+  const append = [];
+  while (legs.length && legs[legs.length - 1].destination?.id !== to) {
+    // @ts-ignore
+    append.unshift(legs.pop());
+  }
+
+  // Journey didn't contain the original connection
+  if (legs.length === 0) return;
+
+  // Fetch current best journey
+  const hash = hashLegs(legs);
+  const oldJourney = journeyMap[hash];
+
+  // Journey not found
+  // TODO: maybe insert the journey into the map even if it's not originally there?!
+  if (!oldJourney || !oldJourney.price?.amount) return;
+
+  // No price improvement
+  if (oldJourney.price.amount <= (journey.price?.amount ?? Infinity)) return;
+
+  // Save how the money saving was achieved
+  journey.trick = {
+    prepend,
+    append,
+    oldPrice: oldJourney.price.amount,
+  };
+
+  journeyMap[hash] = journey;
+}
+
+/**
+ * Queries the original connection and possible longer/cheaper ones.
+ *
+ * @param {BaahnStation} from - origin of journey
+ * @param {BaahnStation} to - destination of journey
+ * @param {JourneysOptions} [opt] - journey options
+ * @returns {Promise<Journeys[]>[]}
+ */
+function buildRequests(from: BaahnStation, to: BaahnStation, opt: JourneysOptions = {}): Promise<Journeys>[] {
+  const requests = [];
+  requests.push(journeys(from, to, opt));
+
+  // Extend the start of the journey
+  opt.via = from;
+  for (const newOrigin of adjacentStations(from)) {
+    from = newOrigin;
+    requests.push(journeys(from, to, opt));
+  }
+  from = opt.via;
+
+  // Extend the end of the journey
+  opt.via = to;
+  for (const newDestination of adjacentStations(to)) {
+    to = newDestination;
+    requests.push(journeys(from, to, opt));
+  }
+
+  return requests;
+}
+
+const defaultOptions: JourneysOptions = {
+  stopovers: false,
+  transfers: -1,
+  transferTime: 0,
+  accessibility: 'none',
+  bike: false,
+  walkingSpeed: 'normal',
+  startWithWalking: true,
+  products: {
+    suburban: true,
+    subway: true,
+    tram: true,
+    bus: true,
+    ferry: true,
+    express: true,
+    regional: true,
+  },
+  tickets: false,
+  polylines: false,
+  subStops: true,
+  entrances: true,
+  remarks: true,
+  scheduledDays: false,
+  language: 'en',
+  firstClass: false,
+};
+
+/**
+ * Finds cheaper prices for given journey.
+ *
+ * @param {BaahnStation} from - origin of journey
+ * @param {BaahnStation} to - destination of journey
+ * @param {JourneysOptions} [opt] - journey options
+ * @returns {Promise<BaahnJourney[]>}
+ * @see {@link https://github.com/public-transport/hafas-client/blob/5/docs/journeys.md|hafas-client}
+ */
+async function findJourneys(from: BaahnStation, to: BaahnStation, opt: JourneysOptions = defaultOptions): Promise<BaahnJourney[]> {
+  const config = Object.assign(opt, defaultOptions);
+
+  // Transform BahnCard discount for db-hafas
+  if (typeof config.loyaltyCard === 'number') {
+    config.loyaltyCard = { type: loyaltyCards.BAHNCARD, discount: config.loyaltyCard };
+  }
+
+  if (config.via) {
+    // eslint-disable-next-line no-console
+    console.warn(`The 'via' option cannot be used. ${config.via} was passed.`);
+  }
+
+  // "via" option cannot be used
+  config.via = undefined;
+
+  const requests = buildRequests(from, to, config);
+  const connections = await Promise.allSettled(requests);
+
+  if (connections.length === 0) return [];
+  const originalConnection = connections.shift();
+  if (!originalConnection?.status || originalConnection?.status === 'rejected') {
+    // There is no journey available
+    return [];
+  }
+
+  // Hash the journeys found so that we can later compare
+  // the extended connections with them more quickly.
+  const cheapestJourneys: BaahnJourneyMap = {};
+  for (const journey of originalConnection.value?.journeys ?? []) {
+    if (!journey.price || !journey.price.amount) continue;
+    const hash = hashLegs(journey.legs);
+    cheapestJourneys[hash] = journey;
+  }
+
+  // Check if the extended journeys are cheaper
+  for (const extendedConnections of connections) {
+    if (extendedConnections.status === 'fulfilled') {
+      for (const journey of extendedConnections.value?.journeys ?? []) {
+        update(cheapestJourneys, journey, from, to);
+      }
+    }
+  }
+
+  return Object.values(cheapestJourneys);
+}
+
+export default findJourneys;
